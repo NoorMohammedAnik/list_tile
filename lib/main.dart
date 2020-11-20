d51207f72990f787bd50f88c802a2d5fefb328e9
NOOR MOHAMMED ANIK <noormohammedanik@gmail.com>
2020-11-20 09:52:06 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text("Easy Explanation"),
+              subtitle: Text("Lear everything with easy explanation"),
+              leading: CircleAvatar(
+                child: Icon(Icons.message),
+
+              ),
+              trailing: Icon(Icons.add_a_photo),
+            ),
+            ListTile(
+              title: Text("Easy Explanation"),
+              subtitle: Text("Lear everything with easy explanation"),
+              leading: CircleAvatar(
+                child: Icon(Icons.message),
+
+              ),
+              trailing: Icon(Icons.add_a_photo),
+            ),
+            ListTile(
+              title: Text("Easy Explanation"),
+              subtitle: Text("Lear everything with easy explanation"),
+              leading: CircleAvatar(
+                child: Icon(Icons.message),
+
+              ),
+              trailing: Icon(Icons.add_a_photo),
+            ),
+            ListTile(
+              title: Text("Easy Explanation"),
+              subtitle: Text("Lear everything with easy explanation"),
+              leading: CircleAvatar(
+                child: Icon(Icons.message),
+
+              ),
+              trailing: Icon(Icons.add_a_photo),
+            ),
+            ListTile(
+              title: Text("Easy Explanation"),
+              subtitle: Text("Lear everything with easy explanation"),
+              leading: CircleAvatar(
+                child: Icon(Icons.message),
+
+              ),
+              trailing: Icon(Icons.add_a_photo),
+            ),
+            ListTile(
+              title: Text("Easy Explanation"),
+              subtitle: Text("Lear everything with easy explanation"),
+              leading: CircleAvatar(
+                child: Icon(Icons.message),
+
+              ),
+              trailing: Icon(Icons.add_a_photo),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
